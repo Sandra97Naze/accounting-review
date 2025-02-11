93e081a9ea6781ee6fcd2b60535678e4bb6251bf
Sandra97Naze <caperun@outlook.fr>
2025-02-11 21:26:32 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true
+  reactStrictMode: true,
+  output: 'export', // Pour génération statique
+  trailingSlash: true,
+  
+  // Configuration pour Netlify
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/companies': { page: '/companies' },
+      '/login': { page: '/login' }
+    }
   }
 }
 
