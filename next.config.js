6efae40a72c9b835a4d4b1fc0f7b23521a30c203
Sandra97Naze <caperun@outlook.fr>
2025-02-11 18:44:39 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
   // Autres configurations possibles
+};
 
-module.exports = nextConfig;
+module.exports = nextConfig;  
