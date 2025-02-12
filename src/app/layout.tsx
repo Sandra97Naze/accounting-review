455d6a6f6033b07f25d73a4a57af7c41703b2041
Sandra97Naze <caperun@outlook.fr>
2025-02-13 00:00:20 +0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,18 +5,18 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Application de Révision Comptable',
-  description: 'Application de révision des comptes annuels',
+    title: 'Application de Révision Comptable',
+    description: 'Application de révision des comptes annuels'
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="fr">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="fr">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
