904999ea91060c7428de8626fab569b58601c26a
Sandra97Naze <caperun@outlook.fr>
2025-02-12 21:35:57 +0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
 // src/app/layout.tsx
 import './globals.css';
 import { Inter } from 'next/font/google';
-import { updateCycleData } from '../../utils/cycleUtils';
-// Utilisez const au lieu de var
-const inter = Inter({ subsets: ['latin'] });
+import { updateCycleData } from './utils/cycleUtils';
 
+const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
     title: 'Application de Révision Comptable',
     description: 'Application de révision des comptes annuels'
