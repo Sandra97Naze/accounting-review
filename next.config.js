52c46f3641beb3fa99815befbc46b851a73634a4
Sandra97Naze <caperun@outlook.fr>
2025-02-12 21:38:02 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone',
-  
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+},
+
   webpack: (config, { isServer }) => {
     // Conservez votre configuration existante pour ts-loader
     config.module.rules.push({
