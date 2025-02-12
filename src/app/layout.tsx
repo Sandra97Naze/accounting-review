3261032515ca90034f799eecbdb3fed868f1149f
Sandra97Naze <caperun@outlook.fr>
2025-02-12 23:50:35 +0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,18 @@
-import { Inter } from "next/font/google";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-  title: "Application de Révision Comptable",
-  description: "Application de révision des comptes annuels"
+export const metadata: Metadata = {
+  title: 'Application de Révision Comptable',
+  description: 'Application de révision des comptes annuels',
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="fr">
