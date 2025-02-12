d8d0a69e92f86158b7af0e968cf21756b44a240e
Sandra97Naze <caperun@outlook.fr>
2025-02-12 12:54:39 +0400

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import DashboardPage from '@/components/dashboard/DashboardPage';
+import DashboardPage from '@/components/dashboard/DashboardComponent';
 
 export default function Page() {
   return <DashboardPage />;
