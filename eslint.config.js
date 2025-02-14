c0aef21ab1691feab48e715bc316ecf6496be6ba
Sandra97Naze <caperun@outlook.fr>
2025-02-14 23:53:32 +0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,32 @@
-const { nextConfig } = require('next/core-web-vitals');
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-module.exports = {
-  ...nextConfig,
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'warn', // Transformer en warning
-    '@typescript-eslint/no-explicit-any': 'warn', 
-    'react/no-unescaped-entities': 'off', 
-    'react/jsx-no-comment-textnodes': 'off',
-    'react/jsx-no-undef': 'warn',
-    'react-hooks/exhaustive-deps': 'warn'
-  },
-  ignores: [
-    'node_modules/',
-    '.next/',
-    'dist/',
-    'build/',
-    'public/'
-  ]
-};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn', // Transformer en warning
+      '@typescript-eslint/no-explicit-any': 'warn', 
+      'react/no-unescaped-entities': 'off', 
+      'react/jsx-no-comment-textnodes': 'off',
+      'react/jsx-no-undef': 'warn',
+      'react-hooks/exhaustive-deps': 'warn'
+    },
+    ignores: [
+      'node_modules/',
+      '.next/',
+      'dist/',
+      'build/',
+      'public/'
+    ]
+  }
+];
+
+export default eslintConfig;
