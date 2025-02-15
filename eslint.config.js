34bbbf6c1f75372d83431bd06433fbed9bfb0d8f
Sandra97Naze <caperun@outlook.fr>
2025-02-15 10:43:24 +0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -8,8 +8,18 @@ import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+]
 
 export default [
   ...compat.extends("next/core-web-vitals"),
@@ -36,3 +46,4 @@ export default [
     ]
   }
 ];
+export default eslintConfig
